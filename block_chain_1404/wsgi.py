3c1f5313aefe79e8eee96c66b7181c10d725a5ee
Ruyonga Dan <druyonga@gmail.com>
2023-11-27 17:07:44 +0900

    first commit

---
+++ block_chain_1404/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for block_chain_1404 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'block_chain_1404.settings')
+
+application = get_wsgi_application()
